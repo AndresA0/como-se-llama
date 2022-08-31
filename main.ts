9b566f870a693445dd581e297f0248c7fc15be0e
AndresA0 <112582788+AndresA0@users.noreply.github.com>
2022-08-31 12:44:47 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . # . # .
-        . . . . .
-        . # # # .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("Callese")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showArrow(ArrowNames.NorthEast)
+basic.showIcon(IconNames.No)
+basic.forever(function () {
+    basic.showIcon(IconNames.Sword)
+    basic.showIcon(IconNames.TShirt)
 })
